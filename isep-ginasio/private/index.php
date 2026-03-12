89e1dbf27778c6f273b1acb45256d3ba68d0c5dc
Goncalo029 <1220673@isep.ipp.pt>
2026-03-12 14:30:35 +0000

    Submissão da Ficha 09: Exercício concluído

---
+++ isep-ginasio/private/index.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php'; ?> 
+<?php include 'includes/nav.php'; ?> 
+    <div class="container-fluid">
+        <div class="row">
+            <?php include 'includes/sidebar.php'; ?>
+            <main class="col-md-9 col-lg-10 p-4">
+                <section>
+                    <h2>ISEP Ginásio</h2>
+                    <p>Escolhe uma opção no menu lateral para continuar.</p>
+                </section>
+            </main> 
+        </div> 
+    </div>
+<?php include 'includes/footer.php'; ?> 
